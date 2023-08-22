8e43f6f55c37e7ed33c380b1a1a429fdec11b0c9
serigne power <serignepower23@gmail.com>
2023-08-22 16:24:00 +0200

    inserimento input

--- src/com/seri/Main.java
+++ src/com/seri/Main.java
@@ -1,12 +1,17 @@
 package com.seri;
 
+import java.util.Scanner;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
         // Press Alt+Invio with your caret at the highlighted text to see how
         // IntelliJ IDEA suggests fixing it.
-        System.out.println("Hello and welcome!");
+        System.out.println("dimmi il tuo nome");
+        Scanner input = new Scanner(System.in);
+        String nome = input.nextLine();
+        System.out.println("Hello " + nome + " and welcome!");
 
         // Press Maiusc+F10 or click the green arrow button in the gutter to run the code.
         for (int i = 1; i <= 5; i++) {
