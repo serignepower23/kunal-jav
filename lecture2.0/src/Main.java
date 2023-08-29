1c69d1338a4ecd417129a8a89df4643d8acae334
serigne power <serignepower23@gmail.com>
2023-08-29 16:04:17 +0200

    aggiunta lezione 2

---
+++ lecture2.0/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int salary = 34444555;
+        if(salary > 6){
+            System.out.println("sal");
+        }
+    }
+}
