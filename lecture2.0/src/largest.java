2221762c2a58fcbb4ac4f1fb149c377f86c2def0
serigne power <serignepower23@gmail.com>
2023-08-29 18:45:36 +0200

    aggiunta metodo 2 a file largest

--- lecture2.0/src/largest.java
+++ lecture2.0/src/largest.java
@@ -6,22 +6,42 @@ public class largest {
 
         int a,b,c;
 
-        System.out.printf("inserire primo numero: ");
+
+        //metodo 1
+        System.out.print("inserire primo numero: ");
         a= input.nextInt();
-        System.out.printf("inserisci il secondo numero: ");
+        System.out.print("inserisci il secondo numero: ");
         b= input.nextInt();
         System.out.printf("inserire secondo numero: ");
         c = input.nextInt();
 
-        int max= a;
+        /*int max= a;
 
-        if(b>max){
+        if(b > max){
             max=b;
         }
         if(c>max){
             max=c;
+        }*/
+
+        int max = 0;
+
+        if(a>b){
+            max =a;
+        }else{
+            max=b;
         }
 
-        System.out.printf("il numero più grande è: "+ max);
+        if (c>max){
+            max=c;
+        }
+
+
+
+        System.out.println("il numero più grande è: "+ max);
+
+        //secondo metodo
+        int massimo = Math.max(c,Math.max(a, b));
+        System.out.print("il numero più grande, secondo metodo: "+ massimo);
     }
 }
